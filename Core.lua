360e73be441a4c4086c08f1b27319fcfdd2fc904
cmKhel <43424181+cmKhel@users.noreply.github.com>
2020-01-28 13:24:08 -0500

    Add slash commands + fixes
    Potentially fix frame panel not showing. Added /sdkp to toggle frame panel.

--- Core.lua
+++ Core.lua
@@ -1,6 +1,33 @@
---Create frame for testing (temporary)
-local testFrame = CreateFrame("Frame", "SDKP_TestFrame", UIParent, "BasicFrameTemplateWithInset");
+-- ~=* Create frame for testing on load *=~
+local testFrame = CreateFrame("Frame", "SDKP_TestFrame", UIParent, "BasicFrameTemplate");
 
 -- Methods / Functions for the new Frame Object:
 testFrame:SetSize(600, 600);
-testFrame:SetPoint("CENTER", UIParent, "CENTER"); -- UI put into center of screen
+testFrame:SetPoint("CENTER", UIParent, "CENTER"); -- panel set at center of screen
+testFrame:SetShown(true);
+testFrame:SetFrameLevel(10); -- ensure highest layer visibility so not blocked by other UI elements
+testFrame:SetClampedToScreen(true); -- prevent panel from being off screen
+
+print("Is shown: " .. testFrame:IsShown() .. " " .. "Is visible: " .. testFrame:IsVisible()); -- Debug to show if frame is showing or visible on load.
+
+
+--  ~=* Create slash commands for chat *=~
+local function MyAddonComands(msg, editbox)
+	if !testFrame:IsShown() then
+		testFrame:SetShown(true)
+		print("Showing testFrame...")
+		print("IsShown() == " .. testFrame:IsShown())
+		print("IsVisible() == " .. testFrame:IsVisible())
+	else
+		testFrame:SetShown(false)
+		print("Hiding testFrame...")
+		print("IsShown() == " .. testFrame:IsShown())
+		print("IsVisible() == " .. testFrame:IsVisible())
+	end
+end
+
+SLASH_SDKP = "/sdkp"
+
+SlashCmdList["SDKP"] = MyAddonComands -- add /sdkp to command list
+
+
