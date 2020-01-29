acfa7dd73542fd4f0e369d3702a228cf4793ff31
cmKhel <cmKhel@users.noreply.github.com>
2020-01-28 19:13:54 -0500

    Added buttons
    TODO: add scroll frames, fix slash commands, add ability to move frame

--- Core.lua
+++ Core.lua
@@ -1,9 +1,5 @@
 -- ~=* Create frame for testing on load *=~
-local testFrame = CreateFrame("Frame", "SDKP_TestFrame", UIParent, "BasicFrameTemplate");
-
-local shown = testFrame:IsShown()
-local visible = testFrame:IsVisible()
-
+local testFrame = CreateFrame("Frame", "SDKP_TestFrame", UIParent, "BasicFrameTemplateWithInset");
 
 -- Methods / Functions for the new Frame Object:
 testFrame:SetSize(600, 600);
@@ -12,15 +8,46 @@ testFrame:SetShown(true);
 testFrame:SetFrameLevel(10); -- ensure highest layer visibility so not blocked by other UI elements
 testFrame:SetClampedToScreen(true); -- prevent panel from being off screen
 
-print("Is shown: " .. shown .. " " .. "Is visible: " .. visible); -- Debug to show if frame is showing or visible on load.
+-- Child frames and regions:
+-- Create title text
+testFrame.title = testFrame:CreateFontString(nil, "OVERLAY");
+testFrame.title:SetFontObject("GameFontHighlight");
+testFrame.title:SetPoint("CENTER", testFrame.TitleBg, "CENTER", 0, 0);
+testFrame.title:SetText("Swarm DKP Test Frame");
 
+-- Create buttons
+testFrame.nameBtn = CreateFrame("Button", nil, testFrame, "GameMenuButtonTemplate");
+testFrame.nameBtn:SetPoint("LEFT", testFrame, "TOP", -150, -50);
+testFrame.nameBtn:SetSize(100, 30);
+testFrame.nameBtn:SetText("Name");
+testFrame.nameBtn:SetNormalFontObject("GameFontNormalLarge");
+testFrame.nameBtn:SetHighlightFontObject("GameFontHighlightLarge");
+
+testFrame.classBtn = CreateFrame("Button", nil, testFrame, "GameMenuButtonTemplate");
+testFrame.classBtn:SetPoint("LEFT", testFrame, "TOP", 0, -50);
+testFrame.classBtn:SetSize(100, 30);
+testFrame.classBtn:SetText("Class");
+testFrame.classBtn:SetNormalFontObject("GameFontNormalLarge");
+testFrame.classBtn:SetHighlightFontObject("GameFontHighlightLarge");
+
+testFrame.dkpBtn = CreateFrame("Button", nil, testFrame, "GameMenuButtonTemplate");
+testFrame.dkpBtn:SetPoint("LEFT", testFrame, "TOP", 150, -50);
+testFrame.dkpBtn:SetSize(100, 30);
+testFrame.dkpBtn:SetText("DKP");
+testFrame.dkpBtn:SetNormalFontObject("GameFontNormalLarge");
+testFrame.dkpBtn:SetHighlightFontObject("GameFontHighlightLarge");
+
+-- TODO: Create scroll frames for table choice (left), and for specific table (right)
 
 --  ~=* Create slash commands for chat *=~
 
 SLASH_SDKP = "/sdkp";
 
-local function SDKPfunc(msg)
-	if testFrame:IsShown() == false then
+local function SlashSDKP(msg, editbox)
+	local shown = testFrame:IsShown()
+	local visible = testFrame:IsVisible()
+
+	if shown == false then
 		testFrame:SetShown(true);
 		print("Showing testFrame...");
 		print("IsShown() == " .. shown);
@@ -33,6 +60,4 @@ local function SDKPfunc(msg)
 	end
 end
 
-SlashCmdList["SDKP"] = SDKPfunc; -- add /sdkp to command list
-
-
+SlashCmdList["SDKP"] = SlashSDKP; -- add /sdkp to command list
