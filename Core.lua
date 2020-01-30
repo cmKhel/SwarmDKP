6b96c3792f73531fd05c6c86b01cde686948d8e1
cmKhel <cmKhel@users.noreply.github.com>
2020-01-30 11:41:39 -0500

    Add config.lua
    move menu creation to config.lua, add beginnings of SavedVariables for database

--- Core.lua
+++ Core.lua
@@ -1,44 +1,6 @@
 -- ~=* Create frame for testing on load *=~
 local testFrame = CreateFrame("Frame", "SDKP_TestFrame", UIParent, "BasicFrameTemplateWithInset");
 
--- Methods / Functions for the new Frame Object:
-testFrame:SetSize(600, 600);
-testFrame:SetPoint("CENTER", UIParent, "CENTER"); -- panel set at center of screen
-testFrame:SetShown(true);
-testFrame:SetFrameLevel(10); -- ensure highest layer visibility so not blocked by other UI elements
-testFrame:SetClampedToScreen(true); -- prevent panel from being off screen
-
--- Child frames and regions:
--- Create title text
-testFrame.title = testFrame:CreateFontString(nil, "OVERLAY");
-testFrame.title:SetFontObject("GameFontHighlight");
-testFrame.title:SetPoint("CENTER", testFrame.TitleBg, "CENTER", 0, 0);
-testFrame.title:SetText("Swarm DKP Test Frame");
-
--- Create buttons
-testFrame.nameBtn = CreateFrame("Button", nil, testFrame, "GameMenuButtonTemplate");
-testFrame.nameBtn:SetPoint("LEFT", testFrame, "TOP", -150, -50);
-testFrame.nameBtn:SetSize(100, 30);
-testFrame.nameBtn:SetText("Name");
-testFrame.nameBtn:SetNormalFontObject("GameFontNormalLarge");
-testFrame.nameBtn:SetHighlightFontObject("GameFontHighlightLarge");
-
-testFrame.classBtn = CreateFrame("Button", nil, testFrame, "GameMenuButtonTemplate");
-testFrame.classBtn:SetPoint("LEFT", testFrame, "TOP", 0, -50);
-testFrame.classBtn:SetSize(100, 30);
-testFrame.classBtn:SetText("Class");
-testFrame.classBtn:SetNormalFontObject("GameFontNormalLarge");
-testFrame.classBtn:SetHighlightFontObject("GameFontHighlightLarge");
-
-testFrame.dkpBtn = CreateFrame("Button", nil, testFrame, "GameMenuButtonTemplate");
-testFrame.dkpBtn:SetPoint("LEFT", testFrame, "TOP", 150, -50);
-testFrame.dkpBtn:SetSize(100, 30);
-testFrame.dkpBtn:SetText("DKP");
-testFrame.dkpBtn:SetNormalFontObject("GameFontNormalLarge");
-testFrame.dkpBtn:SetHighlightFontObject("GameFontHighlightLarge");
-
--- TODO: Create scroll frames for table choice (left), and for specific table (right)
-
 --  ~=* Create slash commands for chat *=~
 
 SLASH_SDKP = "/sdkp";
---
+++ config.lua
@@ -0,0 +1,76 @@
+-- ~=* Namespaces *=~
+
+local _, core = ...;
+core.Config = {};
+
+local Config = core.Config;
+local testFrame;
+
+
+-- ~=* Defaults *=~
+local defaults = { -- will be changed later, following guide to learn at the moment
+	theme = {
+		r = 0
+		g = 0.8
+		b = 1
+		hex = "00ccff"
+	}
+}
+
+function Config:Toggle()
+	local menu = testFrame or Config:CreateMenu();
+	menu:SetShown(not menu:IsShown());
+end
+
+
+-- consolidate similar button creation
+function Config:CreateButton(point, relativeFrame, relativePoint, xOffset, yOffset, width, height, text)
+	local btn = CreateFrame("Button", nil, UIConfig, "GameMenuButtonTemplate");
+	btn:SetPoint(point, relativeFrame, relativePoint, xOffset, yOffset);
+	btn:SetSize(width, height);
+	btn:SetText(text);
+	bent:SetNormalFontObject("GameFontNormalLarge");
+	btn:SetHighlightFontObject("GameFontHighlightLarge");
+	return btn;
+end
+
+
+function Config:CreateMenu()
+-- Methods / Functions for the new Frame Object:
+	testFrame:SetSize(600, 600);
+	testFrame:SetPoint("CENTER", UIParent, "CENTER"); -- panel set at center of screen
+	testFrame:SetShown(true);
+	testFrame:SetFrameLevel(10); -- ensure highest layer visibility so not blocked by other UI elements
+	testFrame:SetClampedToScreen(true); -- prevent panel from being off screen
+
+-- Child frames and regions:
+-- Create title text
+	testFrame.title = testFrame:CreateFontString(nil, "OVERLAY");
+	testFrame.title:SetFontObject("GameFontHighlight");
+	testFrame.title:SetPoint("CENTER", testFrame.TitleBg, "CENTER", 0, 0);
+	testFrame.title:SetText("Swarm DKP Test Frame");
+
+	-- Create buttons
+
+	-- Name Button
+	testFrame.nameBtn = self:CreateButton("LEFT", testFrame, "TOP", -150, -50, 100, 30, "Name");
+	
+	testFrame.classBtn = CreateFrame("Button", nil, testFrame, "GameMenuButtonTemplate");
+	testFrame.classBtn:SetPoint("LEFT", testFrame, "TOP", 0, -50);
+	testFrame.classBtn:SetSize(100, 30);
+	testFrame.classBtn:SetText("Class");
+	testFrame.classBtn:SetNormalFontObject("GameFontNormalLarge");
+	testFrame.classBtn:SetHighlightFontObject("GameFontHighlightLarge");
+
+	testFrame.dkpBtn = CreateFrame("Button", nil, testFrame, "GameMenuButtonTemplate");
+	testFrame.dkpBtn:SetPoint("LEFT", testFrame, "TOP", 150, -50);
+	testFrame.dkpBtn:SetSize(100, 30);
+	testFrame.dkpBtn:SetText("DKP");
+	testFrame.dkpBtn:SetNormalFontObject("GameFontNormalLarge");
+	testFrame.dkpBtn:SetHighlightFontObject("GameFontHighlightLarge");
+
+-- TODO: Create scroll frames for table choice (left), and for specific table (right)
+
+	testFrame:Hide();
+	return testFrame;
+end
