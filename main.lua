8601837bb38fc9c18ba2e073dd0d24c0b0a8ad70
cmKhel <cmKhel@users.noreply.github.com>
2020-02-11 13:16:50 -0500

    remove unnecessary files, rename working file

--- main.lua
+++ main.lua
@@ -1,156 +1,96 @@
--- Testing all in one file to see if this will work.
+--[=====[ 
+****************************************
+			CREATE UI
+****************************************
+--]=====]
 
--- ************************************
---              CONFIG 
--- ************************************
--- ~=* Namespaces *=~
-local _, core = ...;
-core.Config = {};
+-- create panel
+local tablePanel = CreateFrame("Frame", "SDKP_tablePanel", UIParent, "BasicFrameTemplateWithInset");
+-- methods / functions for the new Frame Object:
+	tablePanel:SetSize(600, 600);
+	tablePanel:SetPoint("CENTER", UIParent, "CENTER"); -- panel set at center of screen
+	tablePanel:SetShown(true);
+	tablePanel:SetFrameLevel(10); -- ensure highest layer visibility so not blocked by other UI elements
+	tablePanel:SetClampedToScreen(true); -- prevent panel from being off screen
 
-local Config = core.Config;
-local testFrame;
+-- child frames and regions:
+-- create title text
+	tablePanel.title = tablePanel:CreateFontString(nil, "OVERLAY");
+	tablePanel.title:SetFontObject("GameFontHighlight");
+	tablePanel.title:SetPoint("CENTER", tablePanel.TitleBg, "CENTER", 0, 0);
+	tablePanel.title:SetText("Swarm DKP Test Frame");
 
-
--- ~=* Defaults *=~
-local defaults = { -- will be changed later, following guide to learn at the moment
-	theme = {
-		r = 0
-		g = 0.8
-		b = 1
-		hex = "00ccff"
-	}
-}
-
--- ~=* Frame functions *=~
-function Config:GetThemeColor()
-	local c = defaults.theme;
-	return c.r, c.g, c.b, c.hex;
-end
-
--- consolidate similar button creation for test panel
-function Config:CreateButton(point, relativeFrame, relativePoint, xOffset, yOffset, width, height, text)
-	local btn = CreateFrame("Button", nil, UIConfig, "GameMenuButtonTemplate");
-	btn:SetPoint(point, relativeFrame, relativePoint, xOffset, yOffset);
-	btn:SetSize(width, height);
-	btn:SetText(text);
-	btn:SetNormalFontObject("GameFontNormalLarge");
-	btn:SetHighlightFontObject("GameFontHighlightLarge");
-	return btn;
-end
-
--- ~=* Create frame panel for testing on load *=~
-function Config:CreateMenu()
-local testFrame = CreateFrame("Frame", "SDKP_TestFrame", UIParent, "BasicFrameTemplateWithInset");
--- Methods / Functions for the new Frame Object:
-	testFrame:SetSize(600, 600);
-	testFrame:SetPoint("CENTER", UIParent, "CENTER"); -- panel set at center of screen
-	testFrame:SetShown(true);
-	testFrame:SetFrameLevel(10); -- ensure highest layer visibility so not blocked by other UI elements
-	testFrame:SetClampedToScreen(true); -- prevent panel from being off screen
-
--- Child frames and regions:
--- Create title text
-	testFrame.title = testFrame:CreateFontString(nil, "OVERLAY");
-	testFrame.title:SetFontObject("GameFontHighlight");
-	testFrame.title:SetPoint("CENTER", testFrame.TitleBg, "CENTER", 0, 0);
-	testFrame.title:SetText("Swarm DKP Test Frame");
-
-	-- Create buttons
-	-- Name button
-	testFrame.nameBtn = self:CreateButton("LEFT", testFrame, "TOP", -150, -50, 100, 30, "Name");
-	-- Class button
-	testFrame.classBtn = self:CreateButton("LEFT", testFrame, "TOP", 0, -50, 100, 30, "Class");
+	-- create buttons
+	-- name button
+	nameBtn = CreateFrame("Button", nil, tablePanel, "GameMenuButtonTemplate");
+	nameBtn:SetPoint("LEFT", tablePanel, "TOP", -150, -50);
+	nameBtn:SetSize(100, 30);
+	nameBtn:SetText("Name");
+	nameBtn:SetNormalFontObject("GameFontNormalLarge");
+	nameBtn:SetHighlightFontObject("GameFontHighlightLarge");
+	
+	-- class button
+	classBtn = CreateFrame("Button", nil, tablePanel, "GameMenuButtonTemplate");
+	classBtn:SetPoint("LEFT", tablePanel, "TOP", 0, -50);
+	classBtn:SetSize(100, 30);
+	classBtn:SetText("Class");
+	classBtn:SetNormalFontObject("GameFontNormalLarge");
+	classBtn:SetHighlightFontObject("GameFontHighlightLarge");
+	
 	-- DKP button
-	testFrame.dkpBtn = self:CreateButton("LEFT", testFrame, "TOP", 150, -50, 100, 30, "DKP");
+	dkpBtn = CreateFrame("Button", nil, tablePanel, "GameMenuButtonTemplate");
+	dkpBtn:SetPoint("LEFT", tablePanel, "TOP", 150, -50);
+	dkpBtn:SetSize(100, 30);
+	dkpBtn:SetText("DKP");
+	dkpBtn:SetNormalFontObject("GameFontNormalLarge");
+	dkpBtn:SetHighlightFontObject("GameFontHighlightLarge");
+	
+	-- function to sort by button's name on click
+
 
 -- TODO: Create scroll frames for table choice (left), and for specific table (right)
+-- nested frame maybe?
 
-	testFrame:Hide();
-	return testFrame;
-end
+-- hide the panel on load
+	tablePanel:Hide();
 
+--[=====[ 
+****************************************
+			SLASH COMMANDS
+****************************************
+--]=====]
 
--- ************************************
---              INIT 
--- ************************************
-
--- slash command arguments
-core.commands = {
-	["toggle"] = function()
-		testFrame:SetShown(not menu:IsShown());
-	end,
-
-	["help"] = function()
-		print(" ");
-		core:Print("List of slash commands:")
-		core:Print("/toggle - shows DKP window.")
-		core:Print("/help - shows commands.") 
-		print(" ");
-	end,
-
-
-};
-
-function core:Print(...)
-	local hex = select(4, self.Config:GetThemeColor());
-	local prefix = string.format("!cff%s%s|r", hex:upper(), "SwarmDKP: ");
-	DEFAULT_CHAT_FRAME:AddMessage(string.join(" ", prefix, tostringall(...)));
-end
-
--- implement handler for slash commands 
-local function HandleSlashCommands(str)
-	if (#str == 0) then
-		-- entered /sdkp with no additional arguments
-		core.commands.help();
-
-		return
+local function SDKP_SlashHandler(msg, editbox)
+-- pattern matching that skips leading whitespace and whitespace between cmd and args
+-- any whitespace at end of args is retained
+	local _, _, cmd, args = string.find(msg, "%s?(%w+)%s?(.*)")
+	local function getHelp()
+	  	print("|cffffc863SwarmDKP commands:")
+	    print("/sdkp toggle - toggles DKP window")
+	    print("/sdkp help - prints help commands")
 	end
 
-	local args = {};
-	for _, arg in pairs({ string.split(' ', str) }) do
-		if (#arg > 0) then
-			table.insert(args, arg);
-		end
-	end
+	if cmd == "toggle" then
+	print("|cffffc863SwarmDKP: |cfffffffeToggling DKP window")
+    tablePanel:SetShown(not tablePanel:IsShown());
 
-	local path = core.commands; -- needed for updating found table
+	elseif cmd == "help" then
+		getHelp();
 
-	for id, arg in ipairs(args) do
-		arg = string.lower(arg);
-
-		if (path[arg]) then
-			if type((path[arg]) == "function" ) then
-				-- all remaining arguments passed to function 
-				path[arg](select(id + 1, unpack(args)));
-				return;
-
-			elseif (type(path[arg]) == "table") then
-				path = path[arg]; -- another sub-table found
-
-			else
-				-- does not exist
-				core.commands.help();
-				return
-			end
-		else
-			-- does not exist
-			core.commands.help();
-			return
-		end
-	end
+	else
+	-- If not handled above, display some sort of help message
+		getHelp();
+  end
 end
 
-function core:init(event, name)
-	if (name ~= "SwarmDKP") then return end
+SLASH_SDKP1 = "/sdkp"
 
---  ~=* Create slash commands for chat *=~
-	SLASH_SDKP1 = "/sdkp";
-	SlashCmdList.SDKP =  HandleSlashCommands;
-
-	core:Print("Swarm DKP loaded.");
+SlashCmdList["SDKP"] = function(msg)
+	SDKP_SlashHandler(string.lower(msg));
 end
 
-local events = CreateFrame("Frame");
-events:RegisterEvent("ADDON_LOADED");
-events:SetScript("OnEvent", core.init);
 
+
+--[=====[ 
+
+--]=====]
